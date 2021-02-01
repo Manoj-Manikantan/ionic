372aa9044e016c6537e647151ec0efb926084166
Manoj <manojmkt127@gmail.com>
2021-02-01 16:51:00 -0500

    feat: Assignment 1 - Pay calculator created

---
+++ Assignment1/src/components/ExploreContainer.tsx
@@ -0,0 +1,14 @@
+import './ExploreContainer.css';
+
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <div className="container">
+      <strong>Pay Calculator</strong>
+      <p>UI components will go here</p>
+    </div>
+  );
+};
+
+export default ExploreContainer;
