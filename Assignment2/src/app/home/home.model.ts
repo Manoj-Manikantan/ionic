10e587f5b23697aea1df02ec8c120302265a905a
Manoj <manojmkt127@gmail.com>
2021-03-08 23:37:36 -0500

    feat: Patient list screen created

---
+++ Assignment2/src/app/home/home.model.ts
@@ -0,0 +1,5 @@
+export interface Patient{
+    name: string;
+    age: string;
+    phNum: string;
+}
--- Assignment2/src/app/home/home.page.ts
+++ Assignment2/src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Patient } from './home.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  patients: Patient[] = [
+    {
+      name: "John Wick",
+      age: "32",
+      phNum: "6781235566",
+    },
+    {
+      name: "Dave Batista",
+      age: "45",
+      phNum: "9123556612"
+    },
+    {
+      name: "Shane Warne",
+      age: "28",
+      phNum: "6784209996"
+    }
+  ];
+
+
+  constructor() { }
+
+
+  ngOnInit() {
+
+  }
 
 }
